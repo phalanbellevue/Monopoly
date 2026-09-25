cfa2a0db3f62319adf4917548b8f91de2746c311
pmbellevue <phalan.bellevue@gmail.com>
2026-09-25 11:52:59 -0400

    Task 9 - Added JavaDoc comments to Cell class

--- src/edu/towson/cis/cosc442/project1/monopoly/Cell.java
+++ src/edu/towson/cis/cosc442/project1/monopoly/Cell.java
@@ -3,38 +3,81 @@ package edu.towson.cis.cosc442.project1.monopoly;
 public abstract class Cell {
 	private boolean available = true;
 	private String name;
-	protected Player owner;
+	protected Player theOwner;
 
+	/**
+	 * Returns the name.
+	 *
+	 * @return the resulting string
+	 */
 	public String getName() {
 		return name;
 	}
 
-	public Player getOwner() {
-		return owner;
+	/**
+	 * Returns the the owner.
+	 *
+	 * @return the resulting player
+	 */
+	public Player getTheOwner() {
+		return theOwner;
 	}
 	
+	/**
+	 * Returns the price.
+	 *
+	 * @return the resulting numeric value
+	 */
 	public int getPrice() {
 		return 0;
 	}
 
+	/**
+	 * Checks whether the available.
+	 *
+	 * @return true if the condition holds, false otherwise
+	 */
 	public boolean isAvailable() {
 		return available;
 	}
 	
+	/**
+	 * Play actions.
+	 */
 	public abstract void playAction();
 
+	/**
+	 * Sets the available.
+	 *
+	 * @param available flag indicating whether available
+	 */
 	public void setAvailable(boolean available) {
 		this.available = available;
 	}
 	
+	/**
+	 * Sets the name.
+	 *
+	 * @param name name
+	 */
 	void setName(String name) {
 		this.name = name;
 	}
 
-	public void setOwner(Player owner) {
-		this.owner = owner;
+	/**
+	 * Sets the the owner.
+	 *
+	 * @param owner owner
+	 */
+	public void setTheOwner(Player owner) {
+		this.theOwner = owner;
 	}
     
+    /**
+     * To strings.
+     *
+     * @return the resulting string
+     */
     public String toString() {
         return name;
     }
